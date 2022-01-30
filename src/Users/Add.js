ef69ed155477cddeb3d7ad378eb9bb0fa87e01e8
CondorW-PC <rotsteiner@gmail.com>
2022-01-30 12:11:48 +0100

    refactoring

--- src/Users/Add.js
+++ src/Users/Add.js
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Add(props) {
-  const {onInvalidState} = props;
+  const { onInvalidState } = props;
+
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
 
   const [usernameState, setUsernameState] = useState("");
   const [ageState, setAgeState] = useState("");
@@ -15,25 +18,22 @@ export default function Add(props) {
 
   const submitHandler = (event) => {
     event.preventDefault();
-
-    if(ageState <= 0){
+    console.log(nameInputRef.current.value);
+    if (ageState <= 0) {
       onInvalidState(1);
       return;
-    }
-    else if(usernameState.trim().length < 3){
-      onInvalidState(2)
-    }
-    else{
+    } else if (usernameState.trim().length < 3) {
+      onInvalidState(2);
+    } else {
       const userObj = {
-        id: (Math.random())*10,
+        id: Math.random() * 10,
         name: usernameState,
         age: parseInt(ageState),
       };
       props.onDataSubmit(userObj);
-      setUsernameState('');
-      setAgeState('');
+      setUsernameState("");
+      setAgeState("");
     }
-
   };
 
   return (
@@ -47,6 +47,7 @@ export default function Add(props) {
             id="usernameInput"
             type="text"
             value={usernameState}
+            ref={nameInputRef}
           />
         </div>
         <div>
@@ -57,6 +58,8 @@ export default function Add(props) {
             id="ageInput"
             type="number"
             value={ageState}
+            ref={ageInputRef}
+
           />
         </div>
         <button className="block border-2 p-2">Add new User</button>
